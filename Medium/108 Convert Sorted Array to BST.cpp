235c2cdebbb9bbdf4bab4a946baa5d3bbe033ea3
Rasesh Gupta <rasesh.gupta4949@gmail.com>
2021-07-26 16:21:50 +0530

    Create 108 Convert Sorted Array to BST.cpp

---
+++ Medium/108 Convert Sorted Array to BST.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution	{ 
+private:
+	TreeNode* convertToBSTUtil(vector<int>& nums, int left, int right)	{ 
+		if(left > right) return NULL;
+        int mid = left + (right-left)/2;
+
+        TreeNode* node = new TreeNode(nums[mid]);
+        
+        if(left == right) return node;
+            
+        node->left = convertToBSTUtil(nums, left, mid-1);
+        node->right = convertToBSTUtil(nums, mid+1, right);
+
+        return node;
+    }
+
+public:
+	TreeNode* sortedArrayToBST(vector<int>& nums)	{
+        int left = 0, right = nums.size()-1;
+        TreeNode* root = convertToBSTUtil(nums, 0, right);
+        return root;
+    }
+};
