0191c519fd01432f426c6c35a8fba302e229b45d
Rasesh Gupta <rasesh.gupta4949@gmail.com>
2021-08-02 00:25:28 +0530

    Create 685 Redundant Connection 2.cpp

---
+++ Hard/685 Redundant Connection 2.cpp
@@ -0,0 +1,80 @@
+class unionFind {
+public:
+    vector<int> parent;
+    vector<int> root;
+    
+    unionFind(int n) {
+        parent.resize(n+1);
+        root.resize(n+1);
+        for(int i=0; i<=n; i++) {
+            parent[i] = i;
+            root[i] = i;
+        }
+    }
+    
+    int Find(int i)  {
+        if(i == parent[i]) return i;
+        
+        int j = Find(parent[i]);
+        parent[i] = j;
+        return j;
+    }
+    
+    int FindRoot(int i)  {
+        if(i == root[i]) return i;
+        
+        int j = FindRoot(root[i]);
+        root[i] = j;
+        return j;
+    }
+    
+    bool Union(int i, int j)    {
+        int p1 = FindRoot(i);
+        int p2 = FindRoot(j);
+        
+        if(p1 == p2) return true;
+        
+        root[j] = p1;
+        return false;
+    }
+    
+};
+
+class Solution {
+public:
+    vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        
+        unionFind* obj = new unionFind(n);
+        
+        vector<int> nodesToCheck(2,-1);
+        int twoEdges = -1;
+        vector<int> toRemove = {};
+        
+        for(auto edge : edges)   {
+            int x = edge[0];
+            int y = edge[1];
+            
+            if(obj->parent[y] != y && obj->parent[y] != x)  {
+                nodesToCheck = {obj->parent[y],x};
+                twoEdges = y;
+                obj->parent[y] = y;
+            }
+            else    {
+                obj->parent[y] = x;
+                bool sameSet = obj->Union(x,y);
+                if(sameSet) toRemove = {edge[0],edge[1]};
+            }
+        }
+        
+        if(nodesToCheck[0] != -1) {
+            int v1 = nodesToCheck[0];
+            int v2 = nodesToCheck[1];
+
+            if(obj->Find(v1) == obj->Find(twoEdges)) return {v1,twoEdges};
+            else return {v2,twoEdges};
+        }
+        return toRemove;
+        
+    }
+};
